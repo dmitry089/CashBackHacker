85f0adf086129ad4d8ce17000de1e9ddc88456de
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-03-28 17:53:05 +0400

    added branch with new tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,65 +1,64 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAddTheAmount() {
         int amount = 600;
         int actual = service.remain(amount);
         int expected = 400;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void mustEqualsZero() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void mustEqualsOne() {
         int amount = 1;
         int actual = service.remain(amount);
         int expected = 999;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void notEnoughToLimit() {
         int amount = 1400;
         int actual = service.remain(amount);
         int expected = 600;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void moreThanOneLimit() {
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void lessThanZeroByOne() {
-        assertThrows(RuntimeException.class, () -> {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
 
             service.remain(-1);
         });
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void amountEqualToTheBonus() {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 }
